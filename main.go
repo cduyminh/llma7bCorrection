78935f57401a5de3a8013f90c651e635c76b2cbe
laqu <laqu@netcompany.com>
2023-12-22 17:06:45 +0700

    Add endpoint

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
 	"time"
 )
 
 func main() {
 	InitConfig(".")
+	router := gin.Default()
 
 	ctx := context.Background()
 	redisClient := GetRedisClient()
@@ -42,4 +45,24 @@ func main() {
 
 	fmt.Printf("Total time taken: %s\n", elapsedTime)
 
+	router.POST("/upload", func(c *gin.Context) {
+		// Parse the multipart form
+		err := c.Request.ParseMultipartForm(10 << 20) // 10 MB max memory
+		if err != nil {
+			c.String(http.StatusBadRequest, "Parse form err: %s", err.Error())
+			return
+		}
+
+		// Retrieve the file from the form data
+		file, header, err := c.Request.FormFile("upload[]")
+		if err != nil {
+			c.String(http.StatusBadRequest, "Retrieve file err: %s", err.Error())
+			return
+		}
+		defer file.Close() // Always close the file after processing
+
+		c.String(http.StatusOK, "Processed file: %s", header.Filename)
+	})
+
+	router.Run(":8080")
 }
